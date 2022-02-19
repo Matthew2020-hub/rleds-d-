4f82b6fa193c1e92b67556efb9e752cd319335ab
Matthew <akinolatolulope24@gmail.com>
2022-02-19 21:25:46 +0100

    heroku 500 error fix

--- message/views.py
+++ message/views.py
@@ -82,7 +82,7 @@ def contact_us(request):
             subject = 'Contact Form mail ' ,
             message = message,
             from_email= sender,
-            to=
+            recipient_list=
             ['housefree189@gmail.com'],
             fail_silently=False
         )
