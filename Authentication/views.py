86ca280be75a82c82de4fdc93e52ce7ca67db897
Matthew <akinolatolulope24@gmail.com>
2022-04-13 13:15:37 +0100

    fee

--- Authentication/views.py
+++ Authentication/views.py
@@ -323,7 +323,7 @@ class PasswordReset(APIView):
             verify_OTP = get_object_or_404(VerifyCode, code=response)
             five_minutes_ago = timedelta(minutes=5)
             t = timezone.now()
-            current_time = datetime.now()
+            current_time = datetime.now(timezone.utc)
             print(current_time)
             code_time_check  = current_time - verify_OTP.add_time
             if  code_time_check > five_minutes_ago:
