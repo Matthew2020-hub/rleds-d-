21daddb5c772fe5b24a23668587d5896a9db9927
Matthew <akinolatolulope24@gmail.com>
2022-04-22 07:49:43 +0100

    ff

--- Profile/views.py
+++ Profile/views.py
@@ -17,7 +17,10 @@ from rest_framework.decorators import (
 def profile(request):
     if request.method =='GET':
         try:
-            get_user = Token.objects.get_or_create(key= request.auth)
+            # my_token = request.META.get('HTTP_AUTHORIZATION').split()[1]
+            # print
+            print(request.auth)
+            get_user = Token.objects.get(key= request.auth).user
             user = get_object_or_404(User, name=get_user.name)
             email = user.email
             full_name = user.name
