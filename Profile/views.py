03d6f01e4c56aa5d2fd0e9c4071a0613708973b6
Matthew <akinolatolulope24@gmail.com>
2022-04-23 02:59:07 +0100

    frf

--- Profile/views.py
+++ Profile/views.py
@@ -18,7 +18,6 @@ def profile(request):
     if request.method =='GET':
         try:
             # my_token = request.META.get('HTTP_AUTHORIZATION').split()[1]
-            
             get_user = Token.objects.get(key= request.auth.key).user
             user = get_object_or_404(User, name=get_user.name)
             email = user.email
