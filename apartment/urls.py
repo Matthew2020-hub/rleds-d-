c1b9a75964aa16476f90ede24124b522fc444625
Matthew <akinolatolulope24@gmail.com>
2022-05-02 22:55:58 +0100

    wdf3

--- apartment/urls.py
+++ apartment/urls.py
@@ -4,7 +4,7 @@ from .views import (
     ApartmentCreateListAPIView, 
     ApartmentCreateUpdateDestroyAPIView,
     ApartmentListAPIView,
-    ListAllApartmentAPIView
+
 )
 from django.conf import settings
 from django.conf.urls.static import static
@@ -13,7 +13,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/v1/apartment/post/', ApartmentCreateListAPIView.as_view(), name='apartment-post'),
-    path('api/v1/apartment/all/', ListAllApartmentAPIView.as_view(), name='apartment-list'),
     path('api/v1/apartment/<uuid:apartment_id>', ApartmentCreateUpdateDestroyAPIView.as_view(), name='get-apartment'),
     path('api/v1/apartment/search/', ApartmentListAPIView.as_view(), name='apartment-search')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
