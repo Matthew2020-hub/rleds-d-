bac27ec4d01d8720b4247e2c0fd38dd56a073202
Matthew <akinolatolulope24@gmail.com>
2022-05-17 09:20:37 +0100

    password hint refactoring

--- Profile/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.conf import settings
-# Create your models here.
-
-class Profile(models.Model):
-    user_profile = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- Profile/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-class EditProfileSerializer(serializers.Serializer):
-    Full_name = serializers.CharField()
-    Phone_number = serializers.CharField()
-    Email = serializers.EmailField()
-    Location = serializers.CharField()
-    Profile_image = serializers.ImageField()
-    Background_image = serializers.ImageField()
