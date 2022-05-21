c417609d33353db713f0450fd02cae06cd547bec
Matthew <akinolatolulope24@gmail.com>
2022-05-21 11:26:39 +0100

    heroku error still persisting

--- dev/settings.py
+++ dev/settings.py
@@ -106,12 +106,12 @@ MIDDLEWARE = [
 
 # CORS configuration
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'https://spokane-topaz.vercel.app',
-    'htpps://spokane-blinds.netlify.app/'
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+#     'https://spokane-topaz.vercel.app',
+#     'htpps://spokane-blinds.netlify.app/'
 
-]   
+# ]   
 
 ROOT_URLCONF = 'dev.urls'
 
